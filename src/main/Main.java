2a307ad22d59b9154ef86f4d2761477186c98a22
Spocin <spociomaniak@gmail.com>
2020-01-31 16:23:58 +0100

    Added my class with encryption methode

--- src/main/Main.java
+++ src/main/Main.java
@@ -30,5 +30,7 @@ public class Main {
 
         Caesar10.encrypt();
         Caesar10.decrypt();
+
+        Spocin.encrypt();
     }
 }
---
+++ src/main/Spocin.java
@@ -0,0 +1,69 @@
+package main;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+
+public class Spocin {
+
+    public static void encrypt () {
+
+        FileInputStream fis;
+        StringBuilder input = new StringBuilder();
+
+        try {
+            fis = new FileInputStream("msg.txt");
+
+            int c;
+
+            while ((c = fis.read()) != -1) {
+                input.append((char)c);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        char [][] board = {
+                {'a','b','c','d','e'},
+                {'f','g','h','i','j','k'},
+                {'l','m','n','o','p','.'},
+                {'q','r','s','t','u',','},
+                {'v','w','x','y','z',' '}
+        };
+
+        StringBuilder output = new StringBuilder();
+
+        for (int i = 0; i < input.length() ; i++) {
+
+            for (int j = 0; j < board.length; j++) {
+                for (int k = 0; k < board[j].length; k++) {
+
+                    if ((input.charAt(i) == board[j][k]) || (input.charAt(i) == (board[j][k]-32))) {
+                        output.append(j);
+                        output.append(k);
+                    }
+                }
+            }
+
+        }
+
+        FileOutputStream fos;
+        try {
+            fos = new FileOutputStream("Spocin.txt");
+
+            for (int i = 0; i < output.length(); i++) {
+
+                fos.write(output.charAt(i));
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+
+    }
+
+    public static void decrypt () {
+
+    }
+}
