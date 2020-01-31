bb9d1fc5919710574db6f3541eb11ba7b762b1e3
s20881 <@>
2020-01-31 12:51:04 +0100

    nowy szyfr cezara

---
+++ src/zadanie/Main.java
@@ -0,0 +1,10 @@
+package zadanie;
+
+public class Main
+{
+    public static void main(String args[])
+    {
+        Szyfr test = new Szyfr("ABC");
+        System.out.println(test.szyfruj(1));
+    }
+}
